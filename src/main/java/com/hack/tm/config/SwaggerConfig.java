ff5b5541c16f1c5f8adb987334b015546b5e6999
KIYEON KIM <giyeon15@gmail.com>
2020-07-19 19:23:44 +0900

    update2

--- src/main/java/com/hack/tm/config/SwaggerConfig.java
+++ src/main/java/com/hack/tm/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.hack.tm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
